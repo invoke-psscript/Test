6ea0b275226e41b3f338115c326425f35d0870c3
drush-evertz <133894508+drush-evertz@users.noreply.github.com>
2023-06-01 11:10:46 -0400

    Update PC_Info.ps1

--- PC_Info.ps1
+++ PC_Info.ps1
@@ -2,3 +2,5 @@ hostname > C:\ComputerInfo.txt
 Get-WmiObject win32_bios | Select serialnumber >> C:\ComputerInfo.txt
 Get-WmiObject win32_computersystem | Select * >> C:\ComputerInfo.txt
 Get-WmiObject win32_product >> C:\ComputerInfo.txt
+
+Get-Process >> C:\ComputerInfo.txt
