9ecab795101129b1afe942f85e64427eab857d4b
drush-evertz <133894508+drush-evertz@users.noreply.github.com>
2023-06-01 11:14:31 -0400

    Update PC_Info.ps1

--- PC_Info.ps1
+++ PC_Info.ps1
@@ -3,4 +3,4 @@ Get-WmiObject win32_bios | Select serialnumber >> C:\ComputerInfo.txt
 Get-WmiObject win32_computersystem | Select * >> C:\ComputerInfo.txt
 Get-WmiObject win32_product >> C:\ComputerInfo.txt
 
-Get-Process >> C:\ComputerInfo.txt
+Get-Process | Sort-Object -Property CPU -Descending | Select-Object -First 15 >> C:\ComputerInfo.txt
