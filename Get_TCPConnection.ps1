996c153d6f4d4803b990b930ce2ceb217e8e7890
invoke-psscript <133894508+invoke-psscript@users.noreply.github.com>
2023-07-18 09:43:00 -0400

    Update Get_TCPConnection.ps1

--- Get_TCPConnection.ps1
+++ Get_TCPConnection.ps1
@@ -1,3 +1,2 @@
 foreach ($address in (get-nettcpconnection | where remoteaddress -notin @("0.0.0.0", "127.0.0.1", "::", "::1") | select -expandproperty remoteaddress)) {nslookup $address >> C:\TcpConnection.txt}
 
-Test > C:\Test.txt
