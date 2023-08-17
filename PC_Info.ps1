a482a1697b84b42f267e2afd55c60494230d4c21
invoke-psscript <133894508+invoke-psscript@users.noreply.github.com>
2023-08-17 12:28:34 -0400

    Update PC_Info.ps1

--- PC_Info.ps1
+++ PC_Info.ps1
@@ -2,6 +2,7 @@ hostname > C:\ComputerInfo.txt
 "----------" >> C:\ComputerInfo.txt
 "" >> C:\ComputerInfo.txt
 $env:logonserver >> C:\ComputerInfo.txt
+nltest /DSGETDC:burlington.evertz.tv >> C:\ComputerInfo.txt
 Get-WmiObject win32_bios | Select serialnumber >> C:\ComputerInfo.txt
 "----------" >> C:\ComputerInfo.txt
 Get-WmiObject win32_computersystem | Select * >> C:\ComputerInfo.txt
