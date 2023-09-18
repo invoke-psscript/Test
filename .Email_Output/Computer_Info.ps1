8acad905d4b0ec5d36821ceeb8f0a1497ffef8da
drush <drush@evertz.com>
2023-09-18 11:13:29 -0400

    Computer_Info.ps1

--- .Email_Output/Computer_Info.ps1
+++ .Email_Output/Computer_Info.ps1
@@ -1,1 +1,3 @@
+hostname > C:\tmp\Computer_Info.txt
+
 Send-MailMessage -From track@systm.local -To drush@evertz.com -Subject 'Computer Info' -Body "This is the output of the Computer Info script" -SmtpServer evertz-com.mail.protection.outlook.com
