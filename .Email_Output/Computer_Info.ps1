949c789546b4c537225ca005b622c5171475a137
invoke-psscript <133894508+invoke-psscript@users.noreply.github.com>
2023-09-18 14:24:50 -0400

    Create Computer_Info.ps1

---
+++ .Email_Output/Computer_Info.ps1
@@ -0,0 +1,1 @@
+Send-MailMessage -From track@systm.local -To drush@evertz.com -Subject 'Computer Info' -Body "This is the output of the Computer Info script" -SmtpServer evertz-com.mail.protection.outlook.com
