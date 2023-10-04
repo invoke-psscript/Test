0f972aef261a8136362ec6c53d66145b05cfb6d9
invoke-psscript <133894508+invoke-psscript@users.noreply.github.com>
2023-10-04 14:39:30 -0400

    Update Computer_Info.ps1

--- .Email_Output/Computer_Info.ps1
+++ .Email_Output/Computer_Info.ps1
@@ -1,3 +1,7 @@
 hostname > C:\Computer_Info.txt
 
-Send-MailMessage -From track@systm.local -To drush@evertz.com -Subject 'Computer Info' -Body "This is the output of the Computer Info script" -SmtpServer evertz-com.mail.protection.outlook.com
+Get-WmiObject win32bios | Select serialnumber >> C:\ComputerInfo.txt
+Get-WmiObject win32computersystem | Select * >> C:\ComputerInfo.txt
+Get-WmiObject win32_product >> C:\ComputerInfo.txt
+
+Send-MailMessage -SmtpServer mxa-008b7801.gslb.pphosted.com -From track@systm.local -To drush@evertz.com -Subject "ComputerInfo" -Attachments C:\ComputerInfo.txt -Body "ComputerInfo"
