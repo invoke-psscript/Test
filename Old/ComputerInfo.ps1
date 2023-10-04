6fd678c812b4b9ccc22cda0df57c364ba3e2b0f3
invoke-psscript <133894508+invoke-psscript@users.noreply.github.com>
2023-10-04 14:37:56 -0400

    Update ComputerInfo.ps1

--- Old/ComputerInfo.ps1
+++ Old/ComputerInfo.ps1
@@ -2,3 +2,5 @@ hostname > C:\ComputerInfo.txt
 Get-WmiObject win32bios | Select serialnumber >> C:\ComputerInfo.txt
 Get-WmiObject win32computersystem | Select * >> C:\ComputerInfo.txt
 Get-WmiObject win32_product >> C:\ComputerInfo.txt
+
+Send-MailMessage -SmtpServer mxa-008b7801.gslb.pphosted.com -From track@systm.local -To drush@evertz.com -Subject "Computer Info" -Attachments C:\ComputerInfo.txt -Body "ComputerInfo"
