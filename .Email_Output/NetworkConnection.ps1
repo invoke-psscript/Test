346a4431ead336f814434e8270e58208c7cbd716
invoke-psscript <133894508+invoke-psscript@users.noreply.github.com>
2023-10-16 08:55:31 -0400

    Update NetworkConnection.ps1

--- .Email_Output/NetworkConnection.ps1
+++ .Email_Output/NetworkConnection.ps1
@@ -7,9 +7,11 @@ catch {
   $ErrorOutput = $_.Exception.Message
 }
 
-
+$Hostname = hostname
 
 
 $Domain = (Get-DnsClientGlobalSetting).SuffixSearchList[0]
+nltest /DSGETDC:$Domain | Out-File -FilePath C:\5288_IT\Logs\nltest.txt
 
-nltest /DSGETDC:$Domain | Out-File -FilePath C:\5288_IT\Logs
+
+Send-MailMessage -SmtpServer mxa-008b7801.gslb.pphosted.com -From Reports@5288.IT -To drush@evertz.com -Subject "Network Connection" -Attachments C:\5288_IT\Logs\nltest.txt -Body $Hostname
