b265b161278c7bdd2e12432f2c76eca39e33b1f3
invoke-psscript <133894508+invoke-psscript@users.noreply.github.com>
2023-10-17 13:33:29 -0400

    Update NetworkConnection.ps1

--- .Email_Output/NetworkConnection.ps1
+++ .Email_Output/NetworkConnection.ps1
@@ -1,17 +1,64 @@
+# Variables
+
+$filepath = "C:\5288_IT$\Logs"
+
+$hostname = hostname
+
+$domain = (Get-DnsClientGlobalSetting).SuffixSearchList[0]
+
+$attachments = @()
+
+
+# Output
+#-----------------------------------------------------------
+
+# Create a directory for output
+
 try {
-  If (-not (Test-Path C:\5288_IT$))
-    {New-Item -Path "C:\5288_IT$\Logs" -ItemType Directory
-    (Get-Item -Path "C:\5288_IT$").Attributes = "Hidden"}
+    If (-not (Test-Path C:\5288_IT$))
+        {New-Item -Path $filepath -ItemType Directory
+        (Get-Item -Path "C:\5288_IT$").Attributes = "Hidden"}
 }
 catch {
   $ErrorOutput = $_.Exception.Message
+  "directory error`n-----`n`n" + $ErrorOutput | Out-File -FilePath $filepath\errors.txt -Append
 }
 
-$Hostname = hostname
+
+# Remove previous error log
+
+If (Test-Path $filepath\errors.txt){
+    Remove-Item $filepath\errors.txt
+}
 
 
-$Domain = (Get-DnsClientGlobalSetting).SuffixSearchList[0]
-nltest /DSGETDC:$Domain | Out-File -FilePath C:\5288_IT\Logs\nltest.txt
+# nltest
+
+try {
+    $nltest = (nltest /DSGETDC:$domain) 2>> $filepath\errors.txt
+    $nltest | Out-File -FilePath $filepath\folder\t -Append
+    $attachments += "$filepath\nltest.txt"
+}
+catch{
+    $ErrorOutput = $_.Exception.Message
+    "nltest error`n-----`n`n" + $ErrorOutput | Out-File -FilePath $filepath\errors.txt -Append
+}
 
 
-Send-MailMessage -SmtpServer mxa-008b7801.gslb.pphosted.com -From Reports@5288.IT -To drush@evertz.com -Subject "Network Connection" -Attachments C:\5288_IT\Logs\nltest.txt -Body $Hostname
+# Add error log to attachments array
+
+$attachments += "$filepath\errors.txt"
+
+
+# Send email report
+try{
+    Send-MailMessage -SmtpServer mail.$domain -From Reports@5288.IT -To 5288_IT@evertz.com -Subject "Network Connection" -Attachments $attachments -Body $hostname
+}
+catch{
+    try{
+        Send-MailMessage -SmtpServer mxa-008b7801.gslb.pphosted.com -From Reports@5288.IT -To 5288_IT@evertz.com -Subject "Network Connection" -Attachments $attachments -Body $hostname
+    }
+    catch{
+        Send-MailMessage -SmtpServer mxb-008b7801.gslb.pphosted.com -From Reports@5288.IT -To 5288_IT@evertz.com -Subject "Network Connection" -Attachments $attachments -Body $hostname
+    }
+}
