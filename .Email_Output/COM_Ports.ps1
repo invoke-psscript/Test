1b89b457de59b515315f63cbcc0f676531fb4641
invoke-psscript <133894508+invoke-psscript@users.noreply.github.com>
2023-11-03 09:46:54 -0400

    Create COM_Ports.ps1

---
+++ .Email_Output/COM_Ports.ps1
@@ -0,0 +1,76 @@
+# Variables
+
+$filepath = "C:\5288_IT$\Logs"
+
+$hostname = hostname
+
+$domain = (Get-DnsClientGlobalSetting).SuffixSearchList[0]
+
+$attachments = @()
+
+# Output
+#-----------------------------------------------------------
+
+# Create a directory for output
+
+try {
+    If (-not (Test-Path C:\5288_IT$))
+        {New-Item -Path $filepath -ItemType Directory
+        (Get-Item -Path "C:\5288_IT$").Attributes = "Hidden"}
+}
+catch {
+  $ErrorOutput = $_.Exception.Message
+  "directory error`n-----`n`n" + $ErrorOutput | Out-File -FilePath $filepath\errors.txt -Append
+}
+
+
+# Remove previous error log
+
+If (Test-Path $filepath\errors.txt){
+    Remove-Item $filepath\errors.txt
+}
+-----
+
+# Remove COM ports
+try {
+  Get-PnpDevice >> $filepath\COMportsStart.txt
+  Get-PnpDevice | Where { $_.Class -eq "Ports" -and $_.Status -eq "Unknown" } | ForEach {pnputil /remove-device $_.DeviceId}
+  Get-PnpDevice >> $filepath\COMportsEnd.txt
+  attachments += $filepath\COMportsStart.txt
+  attachments += $filepath\COMportsEnd.txt
+}
+catch{
+  $ErrorOutput = $_.Exception.Message
+  "#task error`n-----`n`n" + $ErrorOutput | Out-File -FilePath $filepath\errors.txt -Append
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+-----
+# Add error log to attachments array
+
+$attachments += "$filepath\errors.txt"
+
+
+# Send email report
+try{
+    Send-MailMessage -SmtpServer mail.$domain -From Reports@5288.IT -To 5288_IT@evertz.com -Subject "COM Ports" -Attachments $attachments -Body $hostname
+}
+catch{
+    try{
+        Send-MailMessage -SmtpServer mxa-008b7801.gslb.pphosted.com -From Reports@5288.IT -To 5288_IT@evertz.com -Subject "Network Connection" -Attachments $attachments -Body $hostname
+    }
+    catch{
+        Send-MailMessage -SmtpServer mxb-008b7801.gslb.pphosted.com -From Reports@5288.IT -To 5288_IT@evertz.com -Subject "Network Connection" -Attachments $attachments -Body $hostname
+    }
+}
