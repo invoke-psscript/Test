bfd5e593be4ebb5c5bb160d3d8759bb95d393cef
invoke-psscript <133894508+invoke-psscript@users.noreply.github.com>
2024-01-18 14:48:30 -0500

    Update InternalHosts.ps1

--- .Email_Output/InternalHosts.ps1
+++ .Email_Output/InternalHosts.ps1
@@ -174,10 +174,16 @@ Get-CimInstance -Namespace root/wmi -ClassName wmimonitorid | Select-Object @{Na
 #-----
 $driveroot = Get-PsDrive -PsProvider FileSystem | ForEach-Object {$_.Root} | Get-ChildItem | Where-Object {($_.Name -like "*.txt") -or ($_.Name -like "*.dll")}
 $drivewindows = Get-PsDrive -PsProvider FileSystem | ForEach-Object {"$($_.Root)Windows"} | Get-ChildItem | Where-Object {($_.Name -like "*.txt") -or ($_.Name -like "*.dll")}
+$cachedbroker = Get-ChildItem C:\Users\$env:username\AppData\Local\Packages\Microsoft.AAD.BrokerPlugin_cw5n1h2txyewy
+$cachedidentity = Get-ChildItem C:\Users\$env:username\AppData\Local\Microsoft\IdentityCache -Recurse
+$cachedoneauth = Get-ChildItem C:\Users\$env:username\AppData\Local\Microsoft\OneAuth -Recurse
 #-----
 $filecheck = "`nFILE CHECK",
 "`ndriveroot`n--------------------", $driveroot,
-"`ndrivewindows`n--------------------", $drivewindows
+"`ndrivewindows`n--------------------", $drivewindows,
+"`ncachedbroker`n--------------------", $cachedbroker,
+"`ncachedidentity`n--------------------", $cachedidentity,
+"`ncachedoneauth`n--------------------", $cachedoneauth
 $filecheck | Out-File $folder\"filecheck.txt"
 #-----
 #----------
