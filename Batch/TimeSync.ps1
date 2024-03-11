26269129ea0c7c0ae2c00a3548d8452958d31ec2
invoke-psscript <133894508+invoke-psscript@users.noreply.github.com>
2024-03-11 13:21:43 -0400

    Update TimeSync.ps1

--- Batch/TimeSync.ps1
+++ Batch/TimeSync.ps1
@@ -1,13 +1,17 @@
 Write-Host (Get-Date)
+""
+Write-Host (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation" -Name "DynamicDaylightTimeDisabled")
+""
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation" -Name "DynamicDaylightTimeDisabled" -Value 0
 
 Write-Host (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation" -Name "DynamicDaylightTimeDisabled")
-
+""
 $dnsserver = @()
 
 $dnsserver += (get-dnsclientserveraddress (get-netadapter -physical | where {$_.Status -eq "Up"}).Name | where addressfamily -eq 2).serveraddresses
 
 Write-Host ("DNS Servers are $dnsserver")
-
+""
 $ntp = $dnsserver[0]
 
 Write-Host ("Using $ntp as ntp server")
