0f4c516e4bb267b470faa2e090ba2ebaa35e8b09
invoke-psscript <133894508+invoke-psscript@users.noreply.github.com>
2024-03-11 12:59:16 -0400

    Update TimeSync.ps1

--- Batch/TimeSync.ps1
+++ Batch/TimeSync.ps1
@@ -2,6 +2,8 @@ Write-Host (Get-Date)
 
 $dnsserver = (get-dnsclientserveraddress (get-netadapter -physical | where {$_.Status -eq "Up"}).Name | where addressfamily -eq 2).serveraddresses
 
+Write-Host ("DNS Servers are $dnsserver")
+
 $ntp = $dnsserver[0]
 
 Write-Host "Using $ntp as ntp server"
