adf270ff340c25fc6a5436d2f994d42d0633628b
invoke-psscript <133894508+invoke-psscript@users.noreply.github.com>
2024-03-12 10:20:42 -0400

    Update Msft_Loop.bat

--- Batch/Msft_Loop.bat
+++ Batch/Msft_Loop.bat
@@ -7,10 +7,17 @@ rmdir C:\Users\%username%\AppData\Local\Microsoft\TokenBroker /S /Q
 REM Remove the sign in loop 
 rmdir C:\Users\%username%\AppData\Local\Packages\Microsoft.AAD.BrokerPlugin_cw5n1h2txyewy /S /Q
 
-REM Remove Teams Cache
+REM Microsoft Entra token broker plugin
+REM Behind the scenes, the Microsoft Entra token broker plugin, represented by the Microsoft.AAD.BrokerPlugin.exe background process, facilitates 
+REM authentication on behalf of the WinForms app.
+
+REM Remove Teams Classic Cache
 rmdir C:\Users\%username%\AppData\Roaming\Microsoft\Teams /S /Q
 
-REM Remove Identity Cache
-rmdir C:\Users\%username%\AppData\Local\Microsoft\IdentityCache /S /Q
+REM Remove New MS Teams Cache Files - Reference https://learn.microsoft.com/en-us/microsoftteams/troubleshoot/teams-administration/clear-teams-cache
+rmdir %userprofile%\appdata\local\Packages\MSTeams_8wekyb3d8bbwe\LocalCache\Microsoft\MSTeams /S /Q
 
-start "" "C:\Program Files\Microsoft Office\root\Office16\outlook.exe"
+REM Remove Identity Cache - Logout of Windows and re-login is required thereafter.
+rmdir C:\Users\%username%\AppData\Local\Microsoft\IdentityCache /S /Q
+cls
+REM Option Time if you want to be forced to logout. Any key to continue, or close this window to exit. But you will need to re-login for Identity Cache to work
