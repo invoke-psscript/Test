1deb385891fe67608349b945401efc9d8b13aedc
invoke-psscript <133894508+invoke-psscript@users.noreply.github.com>
2024-03-13 12:24:40 -0400

    Update GetLogs.ps1

--- .Email_Output/GetLogs.ps1
+++ .Email_Output/GetLogs.ps1
@@ -1,7 +1,7 @@
-cat $Args[0] > $env:TEMP\logs.txt
+v:cat $Args[0] > $env:TEMP\logs.txt
 
 $hostname = hostname
 
 $domain = (Get-WmiObject win32_computersystem).domain
 
-Send-MailMessage -SmtpServer mail.$domain -From Reports@5288_IT -To drush@evertz.com -Subject "Logs" -Attachments C:\Users\$env:username\Documents\logs.txt -Body $hostname
+Send-MailMessage -SmtpServer mail.$domain -From Reports@5288_IT -To drush@evertz.com -Subject "Logs" -Attachments $env:TEMP\logs.txt -Body $hostname
