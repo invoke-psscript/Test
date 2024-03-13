d8d625d74dc1d76f27687dafddd755fc7766dea3
invoke-psscript <133894508+invoke-psscript@users.noreply.github.com>
2024-03-13 09:01:10 -0400

    Update GetFile.ps1

--- .Email_Output/Arguments/GetFile.ps1
+++ .Email_Output/Arguments/GetFile.ps1
@@ -9,8 +9,8 @@ param(
 
 $file = (Get-ChildItem $arg1 | Where Name -like ($arg2 + "*")).Name
 
-attachments = @()
+$attachments = @()
 
-attachments += ($arg1\$file)
+$attachments += ($arg1\$file)
 
 Send-MailMessage -SmtpServer ("mail." + (Get-WmiObject win32_computersystem)) -To "drush@evertz.com" -From "Reports@5288.IT" -Subject GetFile -Attachments $attachments
