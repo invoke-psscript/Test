96ae5e25351688b668a2264f1e20c821e4730efb
invoke-psscript <133894508+invoke-psscript@users.noreply.github.com>
2024-03-15 15:12:11 -0400

    Create GetRDP.ps1

---
+++ .Email_Output/GetRDP.ps1
@@ -0,0 +1,9 @@
+Get-ChildItem "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Recurse > "C:\rdp.txt"
+
+$Body = (Get-Content "C:\rdp.txt" -Raw)
+
+$hostname = hostname
+
+$mail = "mail" + (Get-WmiObject win32_computersystem ).Domain
+
+Send-MailMessage -SmtpServer $mail -To "drush@evertz.com" -From "Reports@5288.IT" -Body $Body -Subject $hostname
