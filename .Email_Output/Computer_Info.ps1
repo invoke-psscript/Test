795aab40c2049cb09629e3939c0b72800cb76555
invoke-psscript <133894508+invoke-psscript@users.noreply.github.com>
2024-03-26 08:58:49 -0400

    Delete .Email_Output/Computer_Info.ps1

--- .Email_Output/Computer_Info.ps1
+++
@@ -1,8 +0,0 @@
-hostname > C:\Computer_Info.txt
-Start-Sleep -Seconds 5
-Get-WmiObject win32_bios | Select serialnumber >> C:\Computer_Info.txt
-Start-Sleep -Seconds 5
-Get-WmiObject win32_computersystem | Select * >> C:\Computer_Info.txt
-Start-Sleep -Seconds 5
-
-Send-MailMessage -SmtpServer mxa-008b7801.gslb.pphosted.com -From track@systm.local -To drush@evertz.com -Subject "ComputerInfo" -Attachments C:\Computer_Info.txt -Body "ComputerInfo"
