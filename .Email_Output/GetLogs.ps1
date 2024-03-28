3eabbe77a38e75f5fdfcc18ca18b6f9b43e7040c
invoke-psscript <133894508+invoke-psscript@users.noreply.github.com>
2024-03-28 15:27:28 -0400

    Delete .Email_Output/GetLogs.ps1

--- .Email_Output/GetLogs.ps1
+++
@@ -1,7 +0,0 @@
-v:cat $Args[0] > $env:TEMP\logs.txt
-
-$hostname = hostname
-
-$domain = (Get-WmiObject win32_computersystem).domain
-
-Send-MailMessage -SmtpServer mail.$domain -From Reports@5288_IT -To drush@evertz.com -Subject "Logs" -Attachments $env:TEMP\logs.txt -Body $hostname
