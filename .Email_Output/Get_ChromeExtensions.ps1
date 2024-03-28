51a5e2a28744a4b3224339f3b2ecfaee1cf6129b
invoke-psscript <133894508+invoke-psscript@users.noreply.github.com>
2024-03-28 15:27:52 -0400

    Delete .Email_Output/Get_ChromeExtensions.ps1

--- .Email_Output/Get_ChromeExtensions.ps1
+++
@@ -1,3 +0,0 @@
-Get-ChildItem "C:\Users\$env:username\AppData\Local\Google\Chrome\User Data\Default\Extensions" > C:\Chrome_Extensions.txt
-
-Send-MailMessage -SmtpServer mxa-008b7801.gslb.pphosted.com -From track@systm.local -To drush@evertz.com -Subject "Chrome Extensions" -Attachments C:\Chrome_Extensions.txt -Body "Chrome Extensions"
